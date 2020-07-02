7bcf031427693f46e15d6c3acdb4cae970d001e5
juniorHotes <juniorhotes53@gmail.com>
2020-07-02 16:40:40 -0300

    conexão com o servidor

---
+++ connect/connection.php
@@ -0,0 +1,13 @@
+<?php 
+    // Conexão com o banco de dados 
+    $serve = 'localhost';
+    $user = 'root';
+    $passworld = '';
+    $db = 'andes';
+
+    $connect = mysqli_connect($serve, $user, $passworld, $db);
+
+    if(mysqli_connect_errno()) {
+        die("Falha na conexão com o banco de dados" . mysqli_connect_errno());
+    }
+?>
