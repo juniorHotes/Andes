d2410ff0653e0e26db3a8ebe694764090bd90c0e
juniorHotes <juniorhotes53@gmail.com>
2020-07-02 18:03:04 -0300

    main.css add

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="_css/main.css">
+        <title>Andes</title>
+    </head>
+    <body>
+        <header>
+            <h1>Andes Coffe</h1>
+            <input type="search" >
+        </header>
+    </body>
+</html>
