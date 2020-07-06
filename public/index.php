9f711d8c2d9cb7ccb0b9dc0804ad85c99850b4a2
juniorHotes <juniorhotes53@gmail.com>
2020-07-06 18:11:08 -0300

    pagina de detalhe e finalização de compra

--- public/index.php
+++ public/index.php
@@ -2,13 +2,14 @@
     
     require_once("../connect/connection.php");
     
-    $products = "SELECT * FROM produtos";
+    $products = "SELECT produtoID, nomeproduto, precounitario, imagempequena FROM produtos";
 
     $query = mysqli_query($connect, $products);
 
     if(!$query) {
         die("Falha na consulta ao banco de dados");
     }
+    
 ?>
 
 
@@ -17,24 +18,25 @@
     <head>
         <meta charset="utf-8">
         <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="_css/main.css">
-        <title>Andes</title>
+        <link rel="stylesheet" href="_css/index.css">
+        <link rel="stylesheet" href="_css/products.css">
+        <title>Andes Coffee</title>
     </head>
     <body>
         <?php require_once("partials/header.php"); ?>
 
         <main>
             <ul>
-            <?php 
+            <?php
                 while($pr = mysqli_fetch_assoc($query)) {
             ?>
                 <li>
-                    <a href="productID=" value="<?php echo $pr["produtoID"] ?>">
+                    <a href="product_details.php?product_Id=<?php echo $pr["produtoID"] ?>">
                         <img width="80" src="<?php echo $pr["imagempequena"] ?>" alt="imagem do produto">
                         <h3><?php echo $pr["nomeproduto"] ?></h3>
-                        <span><?php echo "R$ " . $pr["precounitario"] ?></span>
+                        <span><?php echo "R$ " . number_format($pr["precounitario"], 2,",",".") ?></span>
                     </a>
-                    <a class="buy" href="productID=" value="<?php echo $pr["produtoID"] ?>">
+                    <a class="buy" href="buy.php?product_Id=<?php echo $pr["produtoID"] ?>">
                         COMPRAR
                     </a>
                 </li>                    
---
+++ public/product_details.php
@@ -0,0 +1,57 @@
+<?php 
+    
+    require_once("../connect/connection.php");
+
+    if(isset($_GET['product_Id'])) {
+        $productID = $_GET['product_Id'];
+    } else {
+        Header('location: index.php');
+    }
+    
+    $products = "SELECT * FROM produtos WHERE produtoID = {$productID}";
+
+    $query = mysqli_query($connect, $products);
+
+    $details = mysqli_fetch_assoc($query);
+
+    if(!$query) {
+        die("Falha na consulta ao banco de dados");
+    }
+    
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="_css/index.css">
+        <link rel="stylesheet" href="_css/product_details.css">
+        <title>Andes - Detalhes do produto</title>
+    </head>
+    <body>
+        <?php require_once("partials/header.php"); ?>
+
+        <main>
+            <div id="product-details">
+                <ul>
+                    <img src="<?php echo $details['imagemgrande']; ?>" alt="Imagem do produto">
+                    <div id="details">
+                        <h2><?php echo $details['nomeproduto']; ?></h2>
+                        <h4><?php echo $details['descricao']; ?></h4>
+                        <p><?php echo $details['estoque'] . " Únidades disponíveis"; ?></p>
+                        <span><?php echo "R$ " . number_format($details['precounitario'], 2,",",".") ?></span>
+                    </div>
+                    <a class="buy" href="buy.php?product_Id=<?php echo $pr["produtoID"] ?>" target="_black">
+                        COMPRAR
+                    </a>
+                </ul>
+            </div>
+        </main>
+        
+        <?php require_once("partials/footer.php") ?>
+    </body>
+</html>
+
+<?php mysqli_close($connect); ?>
