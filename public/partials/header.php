08e58ab3dcdeb61a947113ddc781257a2e63cdba
juniorHotes <juniorhotes53@gmail.com>
2020-08-15 12:10:56 -0300

    Novo icone de usuário para logar e sair, remoção do php onde mostrava o nome de usuário

--- public/partials/header.php
+++ public/partials/header.php
@@ -6,29 +6,7 @@
         <button></button>
     </form>
 
-    <?php 
-        // Mensagem de saudação para o usuario
-        if(isset($_SESSION["usuario"])){
-            $user = $_SESSION["usuario"];
-
-            $access = "SELECT nomecompleto ";
-            $access .= "FROM clientes ";
-            $access .= "WHERE clienteID = {$user} ";
-            
-            $access_logon = mysqli_query($connect, $access);
-            if(!$access_logon){
-                die("Falha na consulta ao banco de dados");
-            }
-
-            $access_logon = mysqli_fetch_assoc($access_logon);
-            $name = $access_logon["nomecompleto"];
-    ?>
-        <p><?php echo "Olá " . $name ?> - <a href="logout.php">Logout</a></p>
-    <?php } else { ?>
-        <a href="login.php">Logon</a>
-    <?php } ?>
-
-    <div class="favorite-cart-content">
+    <div class="links-content">
         <div id="favorite-content">
             <a href="index.php?favorites=">
                 <img width="42px" src="assets/coracao-header.svg" alt="Favorites" title="Favorites">
@@ -41,6 +19,20 @@
                 <img width="42px" src="assets/carrinho.svg" alt="Cart" title="Cart">
                 <p id="cart-items" class="circle-qdt">0</p>
             </a>
+        </div>
+
+        <div id="user-content">
+            <?php if(isset($_SESSION["usuario"])) { ?> 
+                <a href="logout.php">
+                    <img width="42px" src="assets/user_icon.svg" alt="User" title="User">
+                    <p>Logout</p>
+                </a>
+            <?php } else { ?>
+                <a href="login.php">
+                    <img width="42px" src="assets/user_icon.svg" alt="User" title="User">
+                    <p>Logon</p>
+                </a>
+            <?php } ?>
         </div>
     </div>
 </header>
