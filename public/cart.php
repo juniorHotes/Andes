081d974e5a1e50080f3d23f0d2c871504c9aafa5
juniorHotes <juniorhotes53@gmail.com>
2020-09-04 15:23:22 -0300

    Alterações nos filhos da div product-details para posicionamentos mais respossivos.

--- public/cart.php
+++ public/cart.php
@@ -21,74 +21,79 @@
         <?php require_once("partials/index/header.php"); ?>
 
         <main>
-            <?php if(isset($_COOKIE[$cookie_name])) { ?>
+            <div class="content">
+                <?php if(isset($_COOKIE[$cookie_name])) { ?>
 
-                <h1>Your cart</h1>
+                    <h1>Your cart</h1>
 
-                <?php
-                    for($i = 0; $i < $arraysize; $i++) {
+                    <?php
+                        for($i = 0; $i < $arraysize; $i++) {
 
-                        $product = "SELECT * FROM produtos WHERE produtoID = {$array[$i]}";
-            
-                        $query = mysqli_query($connect, $product);
-                        $details = mysqli_fetch_assoc($query);
-                        
-                        $produtoID      = $details['produtoID'];
-                        $imagempequena  = $details['imagempequena'];
-                        $nomeproduto    = $details['nomeproduto'];
-                        $codigobarra    = $details['codigobarra'];
-                        $estoque        = $details['estoque'];
-                        $precounitario  = $details['precounitario'];                        
-                ?>
+                            $product = "SELECT * FROM produtos WHERE produtoID = {$array[$i]}";
+                
+                            $query = mysqli_query($connect, $product);
+                            $details = mysqli_fetch_assoc($query);
+                            
+                            $produtoID      = $details['produtoID'];
+                            $imagempequena  = $details['imagempequena'];
+                            $nomeproduto    = $details['nomeproduto'];
+                            $codigobarra    = $details['codigobarra'];
+                            $estoque        = $details['estoque'];
+                            $precounitario  = $details['precounitario'];                        
+                    ?>
 
-                <div class="product-details">
-                    <div>
-                        <img src="<?php echo $imagempequena ?>" alt="Imagem do produto">
-                    </div>
-                    <div class="details" >
-                        <div>
+                    <div class="product-details">
+                        <div class="item1">
+                            <img width="69" src="<?php echo $imagempequena ?>" alt="Imagem do produto">
+                        </div>
+                        <div class="item2">
                             <h2><?php echo $nomeproduto ?></h2>
                             <h5><?php echo $estoque . " Available units"; ?></h5>
                             <h4><?php echo "$" . number_format($precounitario, 2,",",".") . " - Unit price" ?></h4>
                         </div>
-                        <div class="inputs">                        
-                            <p id="total">
-                                <?php echo "Total: $" . number_format($precounitario, 2,",",".") ?>
-                            </p>
-                            <div>
-                                <label for="amount">Units</label>
-                                <input id="<?php echo $i ?>" type="number" name="amount" value="1" min="1" max="<?php echo $estoque ?>">
-                            </div>
+                        <div class="item3 inputs">  
+
                             <div class="delete" id="<?php echo $i ?>">
                                 <a href="#">
                                     <img width="24" src="assets/cruz.svg" alt="Delete item" title="Delete item">
                                 </a>
                             </div>
+
+                            <div>
+                                <label for="amount">Units</label>
+                                <input id="<?php echo $i ?>" type="number" name="amount" value="1" min="1" max="<?php echo $estoque ?>">
+                            </div>
+                      
+                            <p id="total">
+                                <?php echo "Total: $" . number_format($precounitario, 2,",",".") ?>
+                            </p>
+                        </div>
+                        <input type="hidden" value="<?php echo $precounitario ?>">
+                    </div>
+                    
+                    <?php } ?>
+                    
+                    <h2 id="total-value">Total value of your cart: </h2>
+
+                    
+                    <div id="checkout">
+                        <div>
+                            <span>
+                                <a class="button-hover" id="keep-buying" href="index.php">Keep buying</a>
+                            </span>
+                        </div>
+                        <div>
+                            <form action="checkout.php" method="POST">
+                                <input type="hidden" name="_total" value="">
+                                <input class="button-hover" type="submit" value="checkout">
+                            </form>
                         </div>
                     </div>
-                    <input type="hidden" value="<?php echo $precounitario ?>">
-                </div>
-                
+
+                <?php } else { ?>
+                    <h1 style="text-align:center">Your shopping cart is empty</h1>
                 <?php } ?>
-                
-                <h2 id="total-value">Total value of your cart: </h2>
-
-                
-                <div id="checkout">
-                    <span>
-                        <a class="button-hover" id="keep-buying" href="index.php">Keep buying</a>
-                    </span>
-
-                    <form action="checkout.php" method="POST">
-                        <input type="hidden" name="_total" value="">
-                        <input class="button-hover" type="submit" value="checkout">
-                    </form>
-                </div>
-
-            <?php } else { ?>
-                <h1 style="text-align:center">Your shopping cart is empty</h1>
-            <?php } ?>
-
+            </div>
         </main>
         <?php require_once("partials/index/footer.php") ?>
     </body>
