2083b7ba6338655f6106498ec1cf917e7d9c7366
juniorHotes <juniorhotes53@gmail.com>
2020-09-18 11:51:57 -0300

    Janela de alerta.

---
+++ public/partials/index/window_alert.php
@@ -0,0 +1,6 @@
+<div class="alert-container hidden-alert">
+    <div class="alert-window">
+        <h3 id="alert-msn"></h3>
+        <button id="alert-button" class="_hover">OK</button>
+    </div>
+</div>
