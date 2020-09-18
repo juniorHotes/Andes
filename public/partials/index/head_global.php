8698b7fe7bab84b1417e5f84f86452db6059e6a5
juniorHotes <juniorhotes53@gmail.com>
2020-09-18 11:50:46 -0300

    Parte, head padão para todas as páginas.

---
+++ public/partials/index/head_global.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="_css/index/index.css">
+<link rel="stylesheet" href="_css/index/alert.css">
