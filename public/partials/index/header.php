0aeee54920734ce173b4421675190d2a45a038ac
juniorHotes <juniorhotes53@gmail.com>
2020-09-29 09:34:49 -0300

    Novas classe e div para melhorar a UI.

--- public/partials/index/header.php
+++ public/partials/index/header.php
@@ -1,5 +1,5 @@
 <header>
-    <a href="index.php"><h1>Andes Coffee</h1></a>
+    <div id="title"><a href="index.php"><h1>Andes Coffee</h1></a></div>
     
     <form action="index.php" method="get">
         <input type="search" name="search" placeholder="Search">
@@ -7,21 +7,21 @@
     </form>
 
     <div class="links-content">
-        <div id="favorite-content">
+        <div id="favorite-content" class="container-links">
             <a href="index.php?favorites=">
                 <img width="42px" src="assets/coracao-header.svg" alt="Favorites" title="Favorites">
                 <p id="favorite-items" class="circle-qdt">0</p>
             </a>
         </div>
 
-        <div id="cart-content">
+        <div id="cart-content" class="container-links">
             <a href="cart.php">
                 <img width="42px" src="assets/carrinho.svg" alt="Cart" title="Cart">
                 <p id="cart-items" class="circle-qdt">0</p>
             </a>
         </div>
 
-        <div id="user-content">
+        <div id="user-content" class="container-links">
             <?php if(isset($_SESSION["usuario"])) { ?> 
                 <a href="logout.php">
                     <img width="42px" src="assets/user_icon.svg" alt="User" title="User">
@@ -40,7 +40,7 @@
                 $is_admin = $_SESSION['nivel'];
                 if($is_admin == 'admin') {
         ?>
-        <div id="config-content">
+        <div id="config-content" class="container-links">
             <a href="admin.php">
                 <img width="42px" src="assets/configuracoes.svg" alt="Configurations" title="Configurations">
             </a>
