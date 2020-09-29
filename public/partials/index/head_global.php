790420a213767249126638494a985cc69e1e3a7e
juniorHotes <juniorhotes53@gmail.com>
2020-09-29 09:35:33 -0300

    Novas tags meta e link.

--- public/partials/index/head_global.php
+++ public/partials/index/head_global.php
@@ -1,4 +1,6 @@
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="_css/index/index.css">
+<link rel="stylesheet" href="_css/index/header.css">
 <link rel="stylesheet" href="_css/index/alert.css">
