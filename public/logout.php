18ce37f1766cb655c38f04cdcd98e25b14f21eaa
juniorHotes <juniorhotes53@gmail.com>
2020-10-01 11:33:31 -0300

    header_global e estilo css de responssividade.

--- public/logout.php
+++ public/logout.php
@@ -13,10 +13,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="_css/index/index.css">
+        <?php require_once("partials/index/head_global.php"); ?>
         <link rel="stylesheet" href="_css/login/login_style.css">
+        <link rel="stylesheet" href="_css/responsive/login.css">
         <title>Andes Coffee</title>
     </head>
     <style>
